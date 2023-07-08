4d1b89ffa0a232af65f179bbf9610756bd909704
abbuthahir <abbuthahir04@gmail.com>
2023-07-08 12:25:32 +0530

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+ loginForm!: FormGroup;
+ signupForm!: FormGroup;
+
+ constructor(private formBuilder: FormBuilder){
+  this.loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+})
+this.signupForm = new FormGroup({
+  username: new FormControl('', Validators.required),
+  email: new FormControl('', Validators.required),
+  password: new FormControl('', Validators.required),
+  gender: new FormControl('', Validators.required),
+  dob: new FormControl('', Validators.required)
+})
+ }
+
+ ngOnInit() {
+  this.signupForm = this.formBuilder.group({
+    username: [''],
+    gender: [''],
+    dob: [''],
+    email: [''],
+    password: ['']
+  });
+ }
+
+ onSubmit() {
+  const formValues = this.signupForm.value;
+  const existingData = localStorage.getItem('signupDetails');
+  let signupDetails = existingData ? JSON.parse(existingData) : [];
+  signupDetails.push(formValues);
+  localStorage.setItem('signupDetails', JSON.stringify(signupDetails));
+  console.log(formValues);
+  
+}
+}
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+selectedProduct: any;
+  products = [
+    {
+      name: 'Television',
+      image: 'https://images.unsplash.com/photo-1577979749830-f1d742b96791?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8dGVsZXZpc2lvbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      price: 22000,
+      description: 'Buy your Television',
+      isHovered: false,
+    },
+    {
+      name: 'Mobile',
+      image: 'https://images.unsplash.com/photo-1523206489230-c012c64b2b48?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aXBob25lfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+      price: 85000,
+      description: 'Buy your Mobile',
+      isHovered: false
+    },
+    {
+      name: 'Headphone',
+      image: 'https://images.unsplash.com/photo-1583394838336-acd977736f90?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aGVhZHBob25lfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+      price: 8000,
+      description: 'Buy your Headphone',
+      isHovered: false
+    },
+    {
+      name: 'Smart Watch',
+      image: 'https://images.unsplash.com/photo-1508685096489-7aacd43bd3b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c21hcnQlMjB3YXRjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      price: 12000,
+      description: 'Buy your smart watch',
+      isHovered: false
+    }
+  ]
+
+  showProductDetails(product: any) {
+    // this.selectedProduct = product;
+    console.log(product); 
+  }
+
+}
